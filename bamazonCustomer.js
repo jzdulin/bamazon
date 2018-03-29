b061d31e46433cf7ca67485ce59a9186f7636918
Joshua Dulin <jzdulin@Joshuas-MacBook-Pro-2.local>
2018-03-28 20:15:01 -0400

    added prompts and git status git status

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,26 +4,103 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-  
+
     user: "root",
-  
+
     password: "root",
     database: "bamazon"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     start();
-  });
+});
 
-  function start() {
+function start() {
     console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Price: $" + res[i].price);
-      }
-      connection.end();
-    //   runSearch();
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Price: $" + res[i].price);
+        }
+        // connection.end();
+        runSearch();
     });
-  }
+}
+
+// var itemArray = [];
+// var listOfItems = function () {
+//     connection.query('SELECT * FROM products' ,function(err, res) {
+//         for (var i = 0; i < res.length; i++) {
+//             itemArray.push(res[i].product_name);
+//         }
+//     })
+// };
+// listOfItems ();
+// console.log(itemArray);
+
+function runSearch() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                //   name: "choice",
+                //   type: "rawlist",
+                //   choices: itemArray,
+                //   function() {
+                //     var choiceArray = [];
+                //     for (var i = 0; i < results.length; i++) {
+                //       choiceArray.push(results[i].id);
+                //     }
+                //     return choiceArray;
+                //   },
+                name: "choice",
+                type: "input",
+                message: "What is the ID number for the item you would like to purchase?"
+            },
+            {
+                name: "amount",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].id === parseInt(answer.choice)) {
+                        chosenItem = results[i];
+                    }
+                    else if (chosenItem === undefined) {
+                        runSearch();
+                        return console.log("------------------------\nPlease select a valid ID\n------------------------")
+                    }
+                }
+                console.log(chosenItem.stock_quantity);
+
+                if (chosenItem.stock_quantity > parseInt(answer.amount)) {
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: (chosenItem.stock_quantity - answer.amount)
+                            },
+                            {
+                                id: chosenItem.id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("Order placed successfully!");
+                            start();
+                        }
+                    );
+                }
+                else {
+                    start();
+                    return console.log("Insufficient quantity!");
+                }
+            });
+    });
+}
+
+
